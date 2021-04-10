c8836e0fba89158c81bdc1fa1f02db2441068010
Jin Moon <jym2584@rit.edu>
2021-04-10 01:08:19 -0400

    formatting

--- CommandLineMain.java
+++ CommandLineMain.java
@@ -6,49 +6,63 @@ public class CommandLineMain {
     /**
      * Add new modules here
      */
-    public static final List<ktane_modules.Module> modules = new LinkedList<>(); static {
+    private static final List<ktane_modules.Module> modules = new LinkedList<>(); static {
         modules.add(new Wires("On the Subject of Wires"));
         modules.add(new Memory("On the Subject of Memory"));
     }
     
-    public static void availableModules() {
+    /**
+     * Prints the current modules
+     */
+    private static void printAvailableModules() {
+        System.out.println("Available modules: ");
         for(int i = 0; i < modules.size(); i++) {
             System.out.println( String.format("   %d. %s", i+1, modules.get(i).getName()) );
-
         }
-    }
-    public static void main(String[] args) {
-        System.out.println("Keep Talking and Nobody Explodes Helper (Version 241) \n" +
-        "-----------------------------------------------------");
-        System.out.println("Type in the number to access an available module\n");
-        System.out.println("Available modules: ");
-        availableModules();
         System.out.println();
-        boolean getInput = true;
+    }
+
+    /**
+     * Driver code
+     */
+    public static void main(String[] args) {
+        System.out.println(
+            "Keep Talking and Nobody Explodes Helper (Version 241) \n" +
+            "---------------------------------------------------------\n" +
+            "Enter a number from the list to access a module \n " +
+            "For this program, you can type:\n" +
+            "   > help, for more information about a current module or print out a list of current modules\n" +
+            "   > quit, to quit the current module or program" 
+            );
+
+        printAvailableModules();
+
         while(true) {
             try {
+                /* Gathering input */
                 Scanner scanner = new Scanner(System.in);
                 System.out.print(">> Enter a module number: ");
                 String input = "";
-                if(getInput) {
-                    input = scanner.nextLine().toLowerCase();
-                    getInput = false;
-                }
+                input = scanner.nextLine().toLowerCase();
 
                 if(input.equals("quit")) {
                     scanner.close();
                     break;
 
                 } else if (input.equals("help")) {
-                    availableModules();
-                    getInput = false;
+                    printAvailableModules();
 
                 /** Run an available module */
                 } else {
                     
                     try {
-                        System.out.println(modules.get(Integer.parseInt(input) - 1).getName());
+                        System.out.println("\nCurrent Module: " + 
+                            modules.get(Integer.parseInt(input) - 1).getName() + 
+                            "\n-------------------------------------------"
+                        );
                         modules.get(Integer.parseInt(input) - 1).run(scanner);
+
+
 
                     } catch (IndexOutOfBoundsException iobe) {
                         System.out.println("Make sure to type in the number that's associated with the module.\n");
@@ -56,10 +70,12 @@ public class CommandLineMain {
                     } catch (NumberFormatException nfe) {
                         System.out.println("Make sure to type in the number that's associated with the module.\n");
                     }
+
                 }
             } catch (NoSuchElementException nsee) {}
-                getInput = true;
         }
+
+        
 
     }
 }
